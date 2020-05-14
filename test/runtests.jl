4ee6d43b94415f6042e39bd644093839b2527cc9
Piotr Babicz <lepnik01@gmail.com>
2020-05-14 19:16:55 +0100

    Add test for Fibonacci search

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using mopkg
+    using mopkg
 import mopkg: fdc, sfdc, find_min_interval
 using Test
 import InteractiveUtils: subtypes
@@ -41,6 +41,20 @@ svltf = Dict(
             tval = [min[2] - 2, min[2] + 3]
             for stval in tval
                 @test find_min_interval(fun, stval)[1] ≤ min[2] ≤ find_min_interval(fun, stval)[2]
+            end
+        end
+    end
+    @testset "Fibonacci Search tests " begin
+        for (fun, min) in svltf
+            tval = [min[2] - 2, min[2] + 3]
+            for stval in tval
+                @testset "Epsilon tests" begin
+                    for optim in subtypes(SVOptMethod)
+                        for tolerance in [1e-2, 1e-4, 1e-6]
+                            @test isapprox(line_optimize(fun, stval; eps=tolerance, method=optim())[1], min[1], atol=tolerance)
+                        end
+                    end
+                end
             end
         end
     end
