e9dff56bc258d3425766523781329ee08072b013
Piotr Babicz <lepnik01@gmail.com>
2020-05-14 19:06:20 +0100

    Add new function

--- src/svopt.jl
+++ src/svopt.jl
@@ -1,6 +1,7 @@
 abstract type SVOptMethod end
 
 struct SVHillClimb <: SVOptMethod end
+struct SVFiboSearch <: SVOptMethod end
 
 "Finite central difference"
 fdc(f, x; h=1e-5) = (f(x+h/2) - f(x-h/2))/h
@@ -47,5 +48,37 @@ function (svhc::SVHillClimb)(f, x0; ϵ, maxiter, dampingfactor=0.5, step=1.0)
         end
     end
     return fn, s
+
+function (svhc::SVFiboSearch)(f, a, b, ε=0.01)
+    L = b - a
+    k = 1
+    #ilość potrzebnych wyliczeń wartości funkcji przy danej dokładności ε
+    FN1 = trunc(Int, 2 * L / ε)
+    i = 2
+    F = [1, 1]
+    while F[i] < FN1
+        append!(F, F[i] + F[i-1])
+        i = i + 1
+    end
+    N = size(F)[1] - 1
+    while k < N
+        l = F[N - k + 1] / F[N + 1]
+        x1 = a + l
+        x2 = b - l
+        if f(x1) < f(x2)
+            b = x2
+        elseif f(x1) > f(x2)
+            a = x1
+        else
+            a = x1
+            b = x2
+        end
+        k = k + 1
+    end
+    
+    return a, b
+end
+
+
 end
 
