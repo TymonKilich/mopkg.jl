5f9c2dd17623a9c4ab062e3a843d4b13744c1596
Piotr Babicz <lepnik01@gmail.com>
2020-05-15 13:41:35 +0100

    remove redundant

--- src/svopt.jl
+++ src/svopt.jl
@@ -29,6 +29,27 @@ function find_min_interval(f, x0; step=0.1, expandfactor=2.0)
     end
 end
 
+function (svhc::SVHillClimb)(f, x0; ϵ, maxiter, dampingfactor=0.5, step=1.0)
+    x, fp = x0, f(x0)
+    s, fs = x0 + step, f(x0 + step)
+    if fs > fp
+        x, s = s, x
+        fp, fs = fs, fp
+        step = -step
+    end
+    i, fn = 0, Inf
+    while abs(fn-fs) ≥ ϵ && i ≤ maxiter
+        i += 1
+        s += step
+        fs = fn
+        fn = f(s)
+        if fn > fs
+            step = -step*dampingfactor
+        end
+    end
+    return fn, s
+end
+
 function (svhc::SVFiboSearch)(f, a, b; eps)
     L = b - a
     k = 1
@@ -58,44 +79,3 @@ function (svhc::SVFiboSearch)(f, a, b; eps)
     
     return a, b
 end
-
-function find_min_interval(f, x0; step=0.1, expandfactor=2.0)
-    a, b = x0, x0 + step
-    fa, fb = f(a), f(b)
-    if fb > fa
-        a, b = b, a
-        fa, fb = fb, fa
-        step = -step
-    end
-    while true
-        c, fc = b + step, f(b + step)
-        if fc > fb
-            return a < c ? (a, c) : (c, a)
-        end
-        a, b = b, c
-        fa, fb = fb, fc
-        step = step*expandfactor
-    end
-end
-
-function (svhc::SVHillClimb)(f, x0; ϵ, maxiter, dampingfactor=0.5, step=1.0)
-    x, fp = x0, f(x0)
-    s, fs = x0 + step, f(x0 + step)
-    if fs > fp
-        x, s = s, x
-        fp, fs = fs, fp
-        step = -step
-    end
-    i, fn = 0, Inf
-    while abs(fn-fs) ≥ ϵ && i ≤ maxiter
-        i += 1
-        s += step
-        fs = fn
-        fn = f(s)
-        if fn > fs
-            step = -step*dampingfactor
-        end
-    end
-    return fn, s
-end
-
