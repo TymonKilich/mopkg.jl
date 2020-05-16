bd53c3a1e16849cfafac6e67c5eabb2aa23ca03c
Dariusz <dariusz.bakowski0718@gmail.com>
2020-05-16 14:07:13 +0200

    add optimization method

--- src/svopt.jl
+++ src/svopt.jl
@@ -1,6 +1,7 @@
 abstract type SVOptMethod end
 
 struct SVHillClimb <: SVOptMethod end
+struct SVSecant <: SVOptMethod end
 
 "Finite central difference"
 fdc(f, x; h=1e-5) = (f(x+h/2) - f(x-h/2))/h
@@ -49,3 +50,32 @@ function (svhc::SVHillClimb)(f, x0; ϵ, maxiter, dampingfactor=0.5, step=1.0)
     return fn, s
 end
 
+function (svhc::SVSecant)(f, a, b; ϵ, maxiter)
+    if f(a)*f(b) >= ϵ
+        println("secant failed")
+        return
+    end
+    
+    a_temp = a
+    b_temp = b
+    for i = 1:maxiter+1
+        g = a_temp - f(a_temp)*(b_temp - a_temp)/(f(b_temp) -f(a_temp))
+        f_g_temp = f(g)
+
+        if f(a_temp)*f_g_temp < ϵ
+            a_temp = a_temp
+            b_temp = g
+        elseif f(b_temp)*f_g_temp < ϵ
+            a_temp = g
+            b_temp = b_temp
+        elseif f_g_temp == ϵ
+            println("found solution")
+            return g
+        else
+            println("Secant method fails")
+            return -1
+        end
+    end
+    x=a_temp - f(a_temp)*(b_temp - a_temp)/(f(b_temp) -f(a_temp))
+    return f(x), x
+end
