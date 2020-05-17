edc548a966990f4e0efda71eb3f8dfe72dc4458c
mikolajos <36952676+mikolajos@users.noreply.github.com>
2020-05-17 23:07:24 +0200

    Dodanie Bliźniaczej funkcji dla metody siecznych
    Metoda siecznych potrzebuje dwóch punktów startowych a nie jednego jak w przypadku reszty metod.

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -7,6 +7,12 @@ function line_optimize(f, x0; eps=1e-3, maxit=1e5, method::SVOptMethod=SVHillCli
     optimizer(f, x0; ϵ=eps, maxiter=maxit)
 end
 
-export line_optimize, SVOptMethod
+function secants_optimize(f,x0,x1;EPS=10^(-4.),maxit=20,method::SVOptMethod=SVSecant())
+	optimizer = method
+	optimizer(f,x0,x1;eps=EPS,N=maxit)
+end	
+ 
+
+export line_optimize, SVOptMethod,secants_optimize
 
 end # module
