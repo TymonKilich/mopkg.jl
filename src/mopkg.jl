46b8831d5310f891a18d320c2213a6a1a6266fcb
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2020-05-17 22:38:48 +0200

    Add files via upload

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,12 +1,10 @@
 module mopkg
-
 include("svopt.jl")
-
-function line_optimize(f, x0; eps=1e-3, maxit=1e5, method::SVOptMethod=SVHillClimb())
-    optimizer = method
-    optimizer(f, x0; ϵ=eps, maxiter=maxit)
-end
-
+function line_optimize(f, x1, x2, x3; eps=0.5, method::SVOptMethod=SVPowell())  
+    optimizer = method   
+    optimizer(f, x1, x2, x3; epsilon=eps)
+end  
+            
 export line_optimize, SVOptMethod
 
 end # module
