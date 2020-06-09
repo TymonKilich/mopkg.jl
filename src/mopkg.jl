a8393dbf805f9bcaca465ed7d41bb198b86ce053
madzias <madzia.smigielska@gmail.com>
2020-06-09 15:23:02 +0200

    Revert "PR - zloty podzial" Need one more try
    This reverts commit c75e3417281a337f4bd0f68f614c5668e49c6e4d.

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -2,9 +2,9 @@ module mopkg
 
 include("svopt.jl")
 
-function line_optimize(f, a, b; eps=1e-3, method::SVOptMethod=SVHillClimb())
+function line_optimize(f, x0; eps=1e-3, maxit=1e5, method::SVOptMethod=SVHillClimb())
     optimizer = method
-    optimizer(f, a, b)
+    optimizer(f, x0; ϵ=eps, maxiter=maxit)
 end
 
 export line_optimize, SVOptMethod
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,49 +4,27 @@ using Test
 import InteractiveUtils: subtypes
 """
 Single variable global test functions – functions with single global minimum
-key is function (lambda), value (x, y) in minimum
+key is function (lambda), value (y, x) in minimum
 """
 svltf = Dict(
-   	(x -> x^2 - 1) => (0.0, -1.0),
-	(x -> x^2 - 2x) => (1.0, -1.0),
-	(x -> 2x^2 - 4x - 6) => (1.0, -8.0),
-	(x -> x^2 + 2x - sin(x)) => (-0.58, -0.27)
-)
+    (x -> x^2 - 1) => (-1.0, 0.0),
+    (x -> x^2/3 + 2x - sin(x)) => (-3.423528818, -3.99083))
 
 @testset "Single variable optimizers" begin
     @testset "General test for SVOptMethods" begin
         for (fun, min) in svltf
-	    a = min[1] - 10
-	    b = min[1] + 10 
+            tval = [min[2] - 2, min[2] + 3]
+            for stval in tval
                 @testset "Epsilon tests" begin
                     for optim in subtypes(SVOptMethod)
-                        for tolerance in [0.5, 0.2, 0.1]
-                            @test isapprox(line_optimize(fun, a, b; eps=tolerance, method=optim())[1], min[1], atol=tolerance)
+                        for tolerance in [1e-2, 1e-4, 1e-6]
+                            @test isapprox(line_optimize(fun, stval; eps=tolerance, method=optim())[1], min[1], atol=tolerance)
                         end
                     end
                 end
+            end
         end
     end
-
-"""Test check the lengh of range"""
-
-    @testset "Range test" begin
-	for (fun, min) in svltf
-	    a = min[1] - 10
-	    b = min[1] + 10 
-	    for optim in subtypes(SVOptMethod)
-                        for tolerance in [0.5, 0.2, 0.1]
-
-                            @test abs(line_optimize(fun, a, b; eps=tolerance, method=optim())[4])-abs(line_optimize(fun, a, b; eps=tolerance, method=optim())[3]) < tolerance 
-
-                        end
-            end
-	end
-    end
-
-
-
-
     @testset "Finite differences tests" begin
         for xval in [-π, -π/2, 0, π/2, π]
             @test isapprox(2cos(2xval), mopkg.fdc(x -> sin(2x), xval), atol=1e-5)
@@ -57,5 +35,13 @@ svltf = Dict(
             @test isapprox(-4sin(2xval), mopkg.sfdc(x -> sin(2x), xval), atol=1e-5)
         end
         @test isapprox(12*0.05^2, mopkg.sfdc(x -> x^4, 0.05), atol=1e-5)
+    end
+    @testset "Find minimum interval" begin
+        for (fun, min) in svltf
+            tval = [min[2] - 2, min[2] + 3]
+            for stval in tval
+                @test find_min_interval(fun, stval)[1] ≤ min[2] ≤ find_min_interval(fun, stval)[2]
+            end
+        end
     end
 end
