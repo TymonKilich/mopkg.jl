8f4e07b72550693c10d0e313b6485e39c9530dec
Norbert Szulc <norbert@not7cd.net>
2020-09-11 16:59:52 +0200

    Add SVGoldenRatio

--- src/svopt.jl
+++ src/svopt.jl
@@ -49,3 +49,31 @@ function (svhc::SVHillClimb)(f, x0; ϵ, maxiter, dampingfactor=0.5, step=1.0)
     return fn, s
 end
 
+struct SVGoldenRatio <: SVOptMethod end
+
+ρ = (sqrt(5) - 1)/2
+
+fw1(a, b) = a + (1 - ρ)*(b - a)
+fw2(a, b) = b - (1 - ρ)*(b - a)
+
+function (svhc::SVGoldenRatio)(f, x0; ϵ, maxiter)
+    a, b = find_min_interval(f, x0)
+    @assert b > a
+    k = 1  # iterations
+    w1 = fw1(a, b)
+    w2 = fw2(a, b)
+
+    while abs(b - a) > ϵ
+        k += 1
+        if f(w1) < f(w2)
+            b = w2
+            w2 = w1
+            w1 = fw1(a, b)
+        else
+            a = w1
+            w1 = w2
+            w2 = fw2(a, b)
+        end
+    end
+    return f(a), a
+end
